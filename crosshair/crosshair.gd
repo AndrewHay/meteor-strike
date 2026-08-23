e075d8538e2655207525821ec7a7232409aa14da
ilmikko <mikko@5x.fi>
2026-08-23 11:37:28 +0200

    Add crosshair

---
+++ crosshair/crosshair.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+		position = event.position
