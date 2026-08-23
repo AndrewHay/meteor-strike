5743534cf90377a5381c0921c721aac2e62f52e8
AndrewHay <andwhay@gmail.com>
2026-08-23 16:37:58 +0200

    Increasing difficulity

--- level/level.gd
+++ level/level.gd
@@ -2,15 +2,32 @@ extends Node2D
 
 const SPAWN_BEYOND_MARGIN := 8.0
 const METEOR_SPEED := 150.0
+const BASE_SPAWN_INTERVAL := 1.0
+const DIFFICULTY_INTERVAL := 20.0
+const METEOR_RATE_MULTIPLIER := 1.2
 
 var meteor_scene = preload("res://meteor/meteor.tscn")
 
+var _difficulty_level := 0
+var _meteor_timer: Timer
+
 func _ready() -> void:
-	var timer = Timer.new()
-	timer.wait_time = 1.0
-	timer.autostart = true
-	timer.timeout.connect(_spawn_meteor)
-	add_child(timer)
+	_meteor_timer = Timer.new()
+	_meteor_timer.wait_time = BASE_SPAWN_INTERVAL
+	_meteor_timer.autostart = true
+	_meteor_timer.timeout.connect(_spawn_meteor)
+	add_child(_meteor_timer)
+
+	var difficulty_timer := Timer.new()
+	difficulty_timer.wait_time = DIFFICULTY_INTERVAL
+	difficulty_timer.autostart = true
+	difficulty_timer.timeout.connect(_increase_difficulty)
+	add_child(difficulty_timer)
+
+func _increase_difficulty() -> void:
+	_difficulty_level += 1
+	_meteor_timer.wait_time = BASE_SPAWN_INTERVAL / pow(METEOR_RATE_MULTIPLIER, _difficulty_level)
+	get_node("Score").show_warning("WARNING! Meteors increasing!")
 
 func _spawn_meteor() -> void:
 	var bases = get_tree().get_nodes_in_group("base")
--- score/score.gd
+++ score/score.gd
@@ -2,16 +2,27 @@ extends CanvasLayer
 class_name Score
 
 @onready var label: Label = $PanelContainer/Label
+@onready var warning_label: Label = $WarningPanel/WarningLabel
+@onready var warning_timer: Timer = $WarningTimer
 
 var resources := 0
 
 func _ready() -> void:
 	add_to_group("score")
+	warning_timer.timeout.connect(_hide_warning)
 	_update_label()
 
 func add_resource(amount: int = 1) -> void:
 	resources += amount
 	_update_label()
+
+func show_warning(message: String, duration: float = 3.0) -> void:
+	warning_label.text = message
+	$WarningPanel.visible = true
+	warning_timer.start(duration)
+
+func _hide_warning() -> void:
+	$WarningPanel.visible = false
 
 func _update_label() -> void:
 	label.text = "Resources: %d" % resources
