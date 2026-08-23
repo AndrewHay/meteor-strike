3e22863269df317da4073176ead037b02b61a767
AndrewHay <andwhay@gmail.com>
2026-08-23 12:42:59 +0200

    fix crosshair

--- crosshair/crosshair.gd
+++ crosshair/crosshair.gd
@@ -1,9 +1,5 @@
 extends Sprite2D
 
-# Called when the node enters the scene tree for the first time.
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		pass
-	elif event is InputEventMouseMotion:
-		# print("Mouse Motion at: ", event.position)
-		position = event.position
+	if event is InputEventMouseMotion:
+		global_position = get_global_mouse_position()
