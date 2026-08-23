abe10281ac24f8e45b047a345b00d153bf60d7ee
ilmikko <mikko@5x.fi>
2026-08-23 16:33:59 +0200

    Ship hull parts

---
+++ hull_part/hull_part.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+const EXPLOSION = preload("res://explosion/explosion.tscn")
+
+func _ready() -> void:
+	add_to_group("base");
+
+func _on_body_entered(body: Node) -> void:
+	if body is RigidBody2D and body.is_in_group("meteor"):
+		var boom = EXPLOSION.instantiate()
+		boom.position = position
+		get_parent().add_child(boom)
+		queue_free()
