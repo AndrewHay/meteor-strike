79b38ff0a3f9096b9704d42a77358fac3c03a4f2
AndrewHay <andwhay@gmail.com>
2026-08-23 12:38:29 +0200

    spinnin

--- meteor/meteor.gd
+++ meteor/meteor.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 func _ready() -> void:
 	add_to_group("meteor");
 	body_entered.connect(_on_body_entered);
+	angular_velocity = randf_range(-4.0, 4.0);
 
 func _on_body_entered(body: Node) -> void:
 	if body is RigidBody2D:
