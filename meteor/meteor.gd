11c34181b927391fae9b927cb5320559011022ae
ilmikko <mikko@5x.fi>
2026-08-23 12:02:46 +0200

    Collision with meteor also destroys bullet

--- meteor/meteor.gd
+++ meteor/meteor.gd
@@ -8,6 +8,7 @@ func _on_body_entered(body: Node) -> void:
 	if body is RigidBody2D:
 		if body.is_in_group("bullet"):
 			print("meteor gone bye bye")
+			body.queue_free()
 			queue_free()
 		if body.is_in_group("house"):
 			body.queue_free()
