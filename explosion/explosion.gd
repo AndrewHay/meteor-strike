76671ea901a058d86d5fea6e8428d6ada0a618e8
ilmikko <mikko@5x.fi>
2026-08-23 16:06:55 +0200

    Add explosion

--- explosion/explosion.gd
+++ explosion/explosion.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export var animation : AnimationPlayer
+@export var particles : GPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation.play("BOOM")
+	particles.emitting = true
+	await animation.animation_finished
+	queue_free()
--- reactor/reactor.gd
+++ reactor/reactor.gd
@@ -1,10 +1,14 @@
 extends RigidBody2D
 
+const EXPLOSION = preload("res://explosion/explosion.tscn")
+
 func _ready() -> void:
 	add_to_group("base");
 
 func _on_body_entered(body: Node) -> void:
 	if body is RigidBody2D and body.is_in_group("meteor"):
-		print("BOOM")
+		var boom = EXPLOSION.instantiate()
+		boom.position = position
+		get_parent().add_child(boom)
 		Game.instance.game_over()
 		queue_free()
