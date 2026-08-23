e1d939946f7d4a8b5275b2e9a31b1f9a5fc0c9b2
ilmikko <mikko@5x.fi>
2026-08-23 11:51:27 +0200

    Add bullet

--- crosshair/crosshair.gd
+++ crosshair/crosshair.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseButton:
+		print("pew")
+	elif event is InputEventMouseMotion:
 		print("Mouse Motion at: ", event.position)
 		position = event.position
